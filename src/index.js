ca759530a1db602e84692e9440f2b756e68220d7
ShadowLoba415 <erica@aubriannakay.com>
2025-04-29 09:58:03 -0600

    Added HTML

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Challenge 2
+let challenge2Element = document.querySelector("#challenge-2");
+challenge2Element.innerHTML = "Matt Delac";
+
+// Challenge 3
+new Typewriter("#challenge-3", {
+  strings: "Matt Delac",
+  autoStart: true,
+  cursor: null,
+  delay: 400
+});
