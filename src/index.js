103357fc39085acf17c21a5f63c4d6c05da3de93
ShadowLoba415 <erica@aubriannakay.com>
2025-04-30 07:41:47 -0600

    added index html and js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-// Challenge 2
-let challenge2Element = document.querySelector("#challenge-2");
-challenge2Element.innerHTML = "Matt Delac";
+function displayJoke(response) {
+  console.log(response.data.answer);
 
-// Challenge 3
-new Typewriter("#challenge-3", {
-  strings: "Matt Delac",
-  autoStart: true,
-  cursor: null,
-  delay: 400
-});
+  new Typewriter("#joke", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 20
+  });
+}
+
+function generateJoke(event) {
+  event.preventDefault();
+  let apiKey = "c41ea0t0206a28395238ff3c5b4e7o2c";
+  let context =
+    "Your are a funny AI Assistant that tells short and sweet jokes. The joke must be provided in HTML format. Example: <p>this is a joke</p>";
+  let prompt = "Generate a short and unique joke a Mexican people. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let jokeElement = document.querySelector("#joke");
+
+  jokeElement.innerHTML = "Generating a joke for you.. please wait";
+
+  console.log("called the AI api");
+  axios.get(apiUrl).then(displayJoke);
+}
+
+let generatorButton = document.querySelector("#generate-joke-button");
+generatorButton.addEventListener("click", generateJoke);
